1d0257eb8dfd2dc790db1020829522378dce896f
jianxin.lai <jianxin.lai@xcalibyte.com>
2022-09-19 14:47:10 +0800

    add libjson for jfe and remove unused files from jfe

---
+++ osprey/libjson/devtools/__init__.py
@@ -0,0 +1,10 @@
+#  
+# Copyright (C) 2021 Xcalibyte (Shenzhen) Limited.
+#  
+
+# Copyright 2010 Baptiste Lepilleur
+# Distributed under MIT license, or public domain if desired and
+# recognized in your jurisdiction.
+# See file LICENSE for detail or copy at http://jsoncpp.sourceforge.net/LICENSE
+
+# module
